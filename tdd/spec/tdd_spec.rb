47e8f469d64bccb833dccee751c26b6ec242099b
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-10-31 12:33:42 -0700

    Write specs for #my_uniq

--- tdd/spec/tdd_spec.rb
+++ tdd/spec/tdd_spec.rb
@@ -1,1 +1,14 @@
 require 'tdd'
+
+Rspec.describe '#my_uniq' do
+  context 'when given a valid argument' do
+    it 'returns an array with uniq values' do
+      expect(my_uniq([1,2,1,3,3])).to eq([1,2,1,3,3].uniq)
+    end
+  end
+  context 'when given an invalid argument' do
+    it 'raises an error' do
+      expect{my_uniq("a")}.to raise_error(ArgumentError)
+    end
+  end
+end
