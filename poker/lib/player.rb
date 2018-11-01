6b12f8697ed0c6825d36cb7ba7bd9e98255574da
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-10-31 17:31:09 -0700

    Create Player class

--- poker/lib/player.rb
+++ poker/lib/player.rb
@@ -1,4 +1,29 @@
 require_relative 'hand'
 
 class Player
+
+  attr_reader :ans
+
+  def initialize(name, money)
+    @name = name
+    @money = money
+  end
+
+  def discard
+    puts "Which card do you want to discard?"
+    @discard = gets.chomp
+
+  end
+
+  def choices
+    puts "Fold(f), See(s), or Raise(r)"
+    @ans = gets.chomp
+  end
+
+  def get_money
+  end
+
+  def get_raise
+  end
+
 end
